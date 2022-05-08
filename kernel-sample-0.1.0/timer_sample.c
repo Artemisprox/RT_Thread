527afcedafbfbc41a0a941682db101c7af67c85b
Artemisprox <2484434180@qq.com>
2022-05-08 18:21:20 +0800

    任务4

--- kernel-sample-0.1.0/timer_sample.c
+++ kernel-sample-0.1.0/timer_sample.c
@@ -16,49 +16,35 @@
 */
 #include <rtthread.h>
 
-/* 定时器的控制块 */
 static rt_timer_t timer1;
 static rt_timer_t timer2;
-static int cnt = 0;
 
-/* 定时器1超时函数 */
-static void timeout1(void *parameter)
+static void timer1out(void *parameter)
 {
-    rt_kprintf("periodic timer is timeout %d\n", cnt);
-
-    /* 运行第10次，停止周期定时器 */
-    if (cnt++ >= 9)
-    {
-        rt_timer_stop(timer1);
-        rt_kprintf("periodic timer was stopped! \n");
-    }
+	static rt_uint16_t count = 0;
+	rt_kprintf("timer1 : %d\n" , count);
+	count ++;
+	if(count >= 9)
+	{
+	rt_timer_stop(timer1);
+	rt_kprintf("time1 stopped\n");
+	}
 }
 
-/* 定时器2超时函数 */
-static void timeout2(void *parameter)
+static void timer2out(void *parameter)
 {
-    rt_kprintf("one shot timer is timeout\n");
+	rt_kprintf("timer2\n");
 }
 
 int timer_sample(void)
 {
-    /* 创建定时器1  周期定时器 */
-    timer1 = rt_timer_create("timer1", timeout1,
-                             RT_NULL, 10,
-                             RT_TIMER_FLAG_PERIODIC);
+	timer1 = rt_timer_create("timer1",timer1out,RT_NULL,10,RT_TIMER_FLAG_PERIODIC);
+	if(timer1 != RT_NULL) rt_timer_start(timer1);
+	
+	timer2 = rt_timer_create("timer2",timer2out,RT_NULL,30,RT_TIMER_FLAG_ONE_SHOT);
+	if(timer2 != RT_NULL) rt_timer_start(timer2);
+	
+	return 0;
+}	
 
-    /* 启动定时器1 */
-    if (timer1 != RT_NULL) rt_timer_start(timer1);
-
-    /* 创建定时器2 单次定时器 */
-    timer2 = rt_timer_create("timer2", timeout2,
-                             RT_NULL,  30,
-                             RT_TIMER_FLAG_ONE_SHOT);
-
-    /* 启动定时器2 */
-    if (timer2 != RT_NULL) rt_timer_start(timer2);
-    return 0;
-}
-
-/* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(timer_sample, timer sample);
