84fe25a4fd5812c926da612a843e29e717f49940
Artemisprox <2484434180@qq.com>
2022-05-11 19:49:02 +0800

    任务3

--- kernel-sample-0.1.0/timeslice_sample.c
+++ kernel-sample-0.1.0/timeslice_sample.c
@@ -19,50 +19,39 @@
 
 #define THREAD_STACK_SIZE	1024
 #define THREAD_PRIORITY	    20
-#define THREAD_TIMESLICE    10
+#define THREAD_TIMESLICE    8
 
-/* 线程入口 */
-static void thread_entry(void* parameter)
+static void thread_entry(void * parameter)
 {
-    rt_uint32_t value;
-    rt_uint32_t count = 0;
-
-    value = (rt_uint32_t)parameter;
-    while (1)
-    {
-        if(0 == (count % 5))
-        {           
-            rt_kprintf("thread %d is running ,thread %d count = %d\n", value , value , count);      
-
-            if(count > 200)
-                return;            
-        }
-         count++;
-     }  
+	rt_uint32_t value = 0;
+	rt_uint32_t count = 0;
+	
+	value = (rt_uint32_t)parameter;
+	while(1)
+	{
+		if( 0 == (count % 5))
+		{
+			rt_kprintf("thread%d : %d \n",value,count);
+			
+			if(count > 300)
+				return ;
+		}
+		count ++;
+	}	
+	
 }
 
-int timeslice_sample(void)
+int timeslice(void)
 {
-    rt_thread_t tid;
-    /* 创建线程1 */
-    tid = rt_thread_create("thread1", 
-                            thread_entry, (void*)1, 
-                            THREAD_STACK_SIZE, 
-                            THREAD_PRIORITY, THREAD_TIMESLICE); 
-    if (tid != RT_NULL) 
-        rt_thread_startup(tid);
-
-
-    /* 创建线程2 */
-    tid = rt_thread_create("thread2", 
-                            thread_entry, (void*)2,
-                            THREAD_STACK_SIZE, 
-                            THREAD_PRIORITY, THREAD_TIMESLICE-5);
-    if (tid != RT_NULL) 
-        rt_thread_startup(tid);
-    return 0;
+	rt_thread_t tid;
+	tid = rt_thread_create("thread1",thread_entry,(void*)1,THREAD_STACK_SIZE,THREAD_PRIORITY,THREAD_TIMESLICE);
+	if(tid != RT_NULL)
+		rt_thread_startup(tid);
+	
+	tid = rt_thread_create("thread2",thread_entry,(void*)2,THREAD_STACK_SIZE,THREAD_PRIORITY,THREAD_TIMESLICE-5);
+	if(tid != RT_NULL)
+		rt_thread_startup(tid);
+	return 0;
 }
 
-/* 导出到 msh 命令列表中 */
-MSH_CMD_EXPORT(timeslice_sample, timeslice sample);
-
+MSH_CMD_EXPORT(timeslice, timeslice);
